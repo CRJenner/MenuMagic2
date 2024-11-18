5baebbea9530bfbb9205b36dd28e32e2129e838e
CJenner <crjdowning@hotmail.com>
2024-11-18 13:06:26 +0000

    Initial Commit

---
+++ MenuMagic2/MenuMagic2App.swift
@@ -0,0 +1,17 @@
+//
+//  MenuMagic2App.swift
+//  MenuMagic2
+//
+//  Created by Chloe Jenner on 18/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MenuMagic2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MenuMagic2Tests/MenuMagic2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MenuMagic2Tests.swift
+//  MenuMagic2Tests
+//
+//  Created by Chloe Jenner on 18/11/2024.
+//
+
+import Testing
+@testable import MenuMagic2
+
+struct MenuMagic2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
